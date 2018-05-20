9f48bdb775733002a352c63f12dd66204d43c851
jokrey <justin.bennett.developer@gmail.com>
2018-05-20 12:43:21 +0200

    switched to swing implementation as default - since javafx is not reliably available in all compiler envs anymore

--- src/jokrey/game/realistic_game/starter/RealisticGame_Stargate_Starter.java
+++ src/jokrey/game/realistic_game/starter/RealisticGame_Stargate_Starter.java
@@ -3,12 +3,12 @@ package jokrey.game.realistic_game.starter;
 import jokrey.game.realistic_game.engines.RealisticGamePipeline;
 import jokrey.game.realistic_game.engines.Realistic_Game_Engine;
 import jokrey.game.realistic_game.engines.Realistic_Stargate_Game;
-import jokrey.utilities.animation.implementations.java_fx.display.JavaFX_FullScreenStarter;
-import jokrey.utilities.animation.implementations.java_fx.pipeline.AnimationDrawerJavaFX;
+import jokrey.utilities.animation.implementations.swing.display.Swing_FullScreenStarter;
+import jokrey.utilities.animation.implementations.swing.pipeline.AnimationDrawerSwing;
 
 public class RealisticGame_Stargate_Starter {
 	public static void main(String[] args) {
-//		Swing_FullScreenStarter.start(new Realistic_Game_Engine(new Realistic_Stargate_Game()), new RealisticGamePipeline(new AnimationDrawerSwing()));
-		JavaFX_FullScreenStarter.start(new Realistic_Game_Engine(new Realistic_Stargate_Game()), new RealisticGamePipeline(new AnimationDrawerJavaFX()));
+		Swing_FullScreenStarter.start(new Realistic_Game_Engine(new Realistic_Stargate_Game()), new RealisticGamePipeline(new AnimationDrawerSwing()));
+//		JavaFX_FullScreenStarter.start(new Realistic_Game_Engine(new Realistic_Stargate_Game()), new RealisticGamePipeline(new AnimationDrawerJavaFX()));
 	}
 }
